a3dea079c72d9a57e0ad4908ede4bb9febc521af
iampork <tang1151983729@qq.com>
2018-07-27 23:27:48 +0800

    yoka_register

---
+++ yoka_register/scripts/enroll.js
@@ -0,0 +1,46 @@
+window.onload=initPage;
+
+var warnings = {
+  "firstname" : {
+    "required": "Please enter in your first name.",
+    "letters" : "Only letters are allowed in a first name.",
+    "err"     : 0
+  },
+  "lastname" : {
+    "required": "Please enter in your last name.",
+    "letters" : "Only letters are allowed in a last name.",
+    "err"     : 0
+  },
+  "email" : {
+    "required": "Please enter in your e-mail address.",
+    "format" : "Please enter your e-mail in the form 'name@domain.com'.",
+    "err"     : 0
+  }
+}
+
+function initPage(){
+	
+}
+
+
+function isNull(e){
+	
+}
+
+
+function isLetter(e){
+	
+}
+
+
+function isRightEmail(e){
+	
+}
+
+function warning(obj,waringtype){
+	
+}
+
+function noWarn(){
+	
+}
