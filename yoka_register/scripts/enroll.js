09cbe5ea99f9f9efcc51f2d0fbce26032f34aede
iampork <tang1151983729@qq.com>
2018-07-30 23:05:37 +0800

    yoka_register

--- yoka_register/scripts/enroll.js
+++ yoka_register/scripts/enroll.js
@@ -19,28 +19,77 @@ var warnings = {
 }
 
 function initPage(){
-	
+	addEventHandler(document.getElementById("firstname"),"blur",isLetter);
+	addEventHandler(document.getElementById("firstname"),"blur",isNull);
+	addEventHandler(document.getElementById("lastname"),"blur",isNull);
+	addEventHandler(document.getElementById("lastname"),"blur",isLetter);
+	addEventHandler(document.getElementById("email"),"blur",isNull);
+	addEventHandler(document.getElementById("email"),"blur",isRightEmail);
 }
 
 
 function isNull(e){
-	
+	var me=getActivatedObject(e);
+	if(me.value==""){
+		warning(me,"required");
+	}else{
+		noWarn(me,"required");
+	}
 }
 
 
 function isLetter(e){
-	
+	var me=getActivatedObject(e);
+	var nonAlphaChars = /[^a-zA-Z]/;
+	if(nonAlphaChars.test(me.value)){
+		warning(me,"letters");
+	}else{
+		noWarn(me,"letters");
+	}
 }
 
 
 function isRightEmail(e){
-	
+	var me=getActivatedObject(e);
+	if(!/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/.test(me.value)){
+		warning(me,"format");
+	}else{
+		noWarn(me,"format");
+	}
 }
 
 function warning(obj,waringtype){
-	
+	var objParent=obj.parentNode;
+	var warning=eval("warnings."+obj.id+"."+waringtype);
+	if(objParent.getElementsByTagName("p").length==0){
+		var p=document.createElement("p");
+		obj.parentNode.appendChild(p);
+		var pText=document.createTextNode(warning);
+		p.appendChild(pText);
+	}else{
+		var p=objParent.getElementsByTagName("p")[0];
+		p.firstChild.nodeValue=warning;
+	}
+	document.getElementById("enroll").disabled=true;
 }
 
-function noWarn(){
+function noWarn(obj,warningtype){
+	if(obj.parentNode.getElementsByTagName("p").length>0){
+		var p=obj.parentNode.getElementsByTagName("p")[0];
+		var currentWarnings=p.firstChild.nodeValue;
+		var warning=eval("warnings."+obj.id+"."+warningtype);
+		if(currentWarnings==warning){
+			obj.parentNode.removeChild(p);
+		}
+	}
 	
+	var fieldsets=document.getElementsByTagName("fieldset");
+	for(var i=0;i<fieldsets.length;i++){
+		var p=obj.parentNode.getElementsByTagName("p");
+		if(p.length>0){
+			document.getElementById("enroll").disabled=true;
+			return;
+		}
+	}
+	document.getElementById("enroll").disabled=false;
 }
