e390bd065e2787e006567fe5371b8acef2dc4612
iampork <tang1151983729@qq.com>
2018-07-31 22:38:08 +0800

    POST方法

--- Mike'sMovies/scripts/utils.js
+++ Mike'sMovies/scripts/utils.js
@@ -14,3 +14,27 @@ function createRequest() {
   }	
   return request;
 }
+
+
+function getActivatedObject(e) {
+  var obj;
+  if (!e) {
+    // early version of IE
+    obj = window.event.srcElement;
+  } else if (e.srcElement) {
+    // IE 7 or later
+    obj = e.srcElement;
+  } else {
+    // DOM Level 2 browser
+    obj = e.target;
+  }
+  return obj;
+}
+
+function addEventHandler(obj, eventName, handler) {
+  if (document.attachEvent) {
+    obj.attachEvent("on" + eventName, handler);
+  } else if (document.addEventListener) {
+    obj.addEventListener(eventName, handler, false);
+  }
+}
